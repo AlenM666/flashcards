c90f9d57835f84ea621fe0618222e7d1dd51ceff
Jane <jane@pop-os.localdomain>
2022-10-10 21:11:13 +0200

    added main.py

---
+++ min.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+saved_memory_cards = []
+
+def card():
+    card_content = input("Enter What you want memorised: ")
+    saved_memory_cards.append(card_content)
+    
+    
+
+print(card())
+
+
+
